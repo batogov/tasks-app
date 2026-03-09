b014fff126f30b6e0f43586f2315d229a2a3f83b
Dmitrii Batogov <batogovdm@yandex.ru>
2026-03-09 21:05:05 +0300

    Initialize Xcode project

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Dmitriy Batogov on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
